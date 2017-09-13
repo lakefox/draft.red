5e92672343879e86b3da9bcd051dc7c06b366743
LakeFox <mason@lakefox.net>
2017-09-13 07:15:43 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 document.querySelector("#start").addEventListener("keyup", (e)=>{
   if (e.key == "Enter") {
     document.querySelector("#landing").style.display = "none";
+    document.querySelector("#start").style.top = "35px";
+    document.querySelector("#start").style.left = "135px";
+    document.querySelector("#start").setAttribute("readonly","");
   }
 });
