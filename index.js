a33f4aaccf6a0819663bfe1e3a131b7c684941a2
LakeFox <mason@lakefox.net>
2017-09-12 21:53:15 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,5 @@
-function main() {
-  if (detectmob()) {
-  	// Is on mobile
-    // Hide the desktop GUI
-    document.querySelector("#landing-desktop").style.display = "none";
-    if (window.location.hash.length > 1) {
-      // Playing
-      playMobile();
-    }
-  } else {
-  	// Is on desktop
-   // Hide the mobile GUI
-   document.querySelector("#landing-mobile").style.display = "none";
-    if (window.location.hash.length > 1) {
-      // Playing
-      playDesktop();
-    }
-  }
-}
-main();
-function playDesktop() {
-  document.querySelector("#landing-desktop").style.display = "none";
-  document.querySelector("#portal-desktop").style.display = "inherit";
-}
-function playMobile() {
-  document.querySelector("#landing-mobile").style.display = "none";
-  document.querySelector("#portal-mobile").style.display = "inherit";
-}
-function start() {
-
-}
-function detectmob() {
-  if(window.innerWidth <= 600 && window.innerHeight <= 800) {
-    return true;
-  } else {
-    return false;
-  }
-}
-document.querySelector("#gameid").addEventListener("keydown", (e)=>{
+document.querySelector("#start").addEventListener("keyup", (e)=>{
   if (e.key == "Enter") {
-    window.location.hash = document.querySelector("#gameid").value;
-    window.location.reload();
+    document.querySelector("#landing").style.display = "none";
   }
-})
+});
