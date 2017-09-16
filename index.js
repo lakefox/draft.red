b6b4fde17b5777d50d6aaf46da2e28b5813b0d2c
LakeFox <mason@lakefox.net>
2017-09-16 15:43:40 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var named = false
 document.querySelector("#start").addEventListener("keyup", (e)=>{
   if (e.key == "Enter") {
     document.querySelector("#landing").style.display = "none";
@@ -6,5 +7,16 @@ document.querySelector("#start").addEventListener("keyup", (e)=>{
     document.querySelector("#start").setAttribute("readonly","");
     document.querySelector("#start").setAttribute("class","tab");
     document.querySelector("#portal").style.display = "inherit";
+    named = true;
   }
 });
+var show = "myteam";
+function tab(e) {
+  if (named) {
+    if (show) {
+      document.querySelector("#"+show).style.display = "none";
+    }
+    show = e.getAttribute("data-show");
+    document.querySelector("#"+show).style.display = "inherit";
+  }
+}
