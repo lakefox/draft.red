584614ac4a9a11272cdf4d2849a474c438b17b61
LakeFox <mason@lakefox.net>
2017-10-09 07:31:06 -0500

    Add files via upload

--- dred.js
+++ dred.js
@@ -7,15 +7,14 @@ function dred(t1, t2) {
   var down = 1;
   console.log(off);
   var kicker = rate(teams[coinflip].k[0]);
-  var dist = (Math.floor((random()+kicker)*6)+1)*10; // calc kick
+  var dist = Math.min((Math.round((random()+kicker)*6)+1)*10, 70); // calc kick
   if (dist < line) {
     // Turn over
     off = Math.round(Math.abs(Math.min(Math.max(off,0.1),1)-1)); // Lol $) This just inverts the off
-    down += 1;
   } else {
     line += dist;
   }
-  console.log(dist,off);
+  console.log(dist,off,line);
   // First half
   for (var q = 0; q < 2; q++) {
     for (var p = 0; p < 12; p++) {
@@ -26,6 +25,6 @@ function dred(t1, t2) {
     var max = Math.max(p.speed,p.strength,p.endurance,p.agilty);
     var sum = p.speed+p.strength+p.endurance+p.agilty;
     var base = ((((sum-max)/3)+max)*(38-p.age));
-    return parseFloat("0."+Math.floor(((((sum-max)/3)+max)/base)*100));
+    return (((sum-max)/3)+max)/base;
   }
 }
