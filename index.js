636c4ff455ec03c51af5fe0cc407f551c5c7ad77
LakeFox <mason@lakefox.net>
2017-11-13 19:56:05 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -9,10 +9,21 @@
     }
     last = window.scrollY;
     var s = window.scrollY%window.innerHeight;
-    console.log(s);
     if (s+70 > window.innerHeight && direction) {
       var to = (Math.floor(window.scrollY/window.innerHeight)*window.innerHeight)+window.innerHeight;
       window.scrollTo(0,to);
     }
   });
 })()
+function media(e) {
+  var action = e.innerHTML.trim();
+  var movies = document.querySelector(".movies");
+  var show = document.querySelector(".show");
+  if (action == "NEXT" && show.nextElementSibling) {
+    show.nextElementSibling.setAttribute("class", "movie show");
+    show.setAttribute("class", "movie");
+  } else if (action == "PREV" && show.previousElementSibling) {
+    show.previousElementSibling.setAttribute("class", "movie show");
+    show.setAttribute("class", "movie");
+  }
+}
